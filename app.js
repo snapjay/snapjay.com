1357b348fba875cda95259e1f1cd9a144fb3f276
Dan Shreim <dan@snapjay.com>
2014-12-08 11:28:31 -0500

    Re-enable static path

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -24,18 +22,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    res.render('index.html', { title: 'Express' });
+    res.render('index.html', { title: 'snapjay.com' });
 });
 
 // error handlers
-
+console.log (app.get('env'));
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
