61de6ad872d69ea2ea6dce297d6642af4d31b000
Dan Shreim <dan@snapjay.com>
2018-10-12 17:13:42 -0400

    Add Play Links

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -9,46 +9,52 @@
         .row.justify-content-md-center
             .col-md-4.viewport
                 router-link(:to='{name: "play", params: {active: "county"} }')
-                    img(src='/static/img/play/boot.svg', alt='Boot')
+                    svg.item(viewBox='0 0 100 100')
+                        use(xlink:href='#svg_boot')
                 router-link(:to='{name: "play", params: {active: "cooking"} }')
-                    img(src='/static/img/play/cooking.svg', alt='Cooking')
+                    svg.item(viewBox='0 0 100 100')
+                        use(xlink:href='#svg_cooking')
                 router-link(:to='{name: "play", params: {active: "food"} }')
-                    img(src='/static/img/play/food.svg', alt='Food')
+                    svg.item(viewBox='0 0 100 100')
+                        use(xlink:href='#svg_food')
                 router-link(:to='{name: "play", params: {active: "football"} }')
-                    img(src='/static/img/play/football.svg', alt='Football')
+                    svg.item(viewBox='0 0 100 100')
+                        use(xlink:href='#svg_football')
                 router-link(:to='{name: "play", params: {active: "offroad"} }')
-                    img(src='/static/img/play/jeep.svg', alt='Off Roading')
+                    svg.item(viewBox='0 0 100 100')
+                        use(xlink:href='#svg_jeep')
                 router-link(:to='{name: "play", params: {active: "hockey"} }')
-                    img(src='/static/img/play/hockey.svg', alt='Hockey')
+                    svg.item(viewBox='0 0 100 100')
+                        use(xlink:href='#svg_hockey')
 
-                div(v-if='active === "hockey"', role='presentation')
+                div(v-if='active === "hockey"')
                     h3 Ice Hockey
                     p I've been playing hockey for over 10 years.
                     p I've played in leagues, tournaments and recreational, but prefer now a relaxed pick-up.
                     p In winter I'll most likely be found playing under the stars on a pond.
 
-                div(v-if='active === "cooking"', role='presentation')
+                div(v-if='active === "cooking"')
                     h3 Cooking
                     p I'm passionate about cooking and good food; I rarely purchase products that have a nutrition label; I like to create everything from scratch using raw ingredients.
                     p I'll make my own butter, mayonnaise, soups, flour, chili, pies, breads etc.
 
-                div(v-if='active === "football"', role='presentation')
+                div(v-if='active === "football"')
                     h3 Football
-                    p In 2010 I co-founded the TGFL. Canada's first LGBT flag football league.
+                    p In 2010 I co-founded the #[a(href='http://www.torontogayfootball.com/', target='_blank') TGFL]. Canada's first LGBT flag football league.
                     p While I no longer serve on the executive, the league is still going strong with 2 divisions, 14 teams and over 180 players providing an inclusive environment for people to play football.
 
-                div(v-if='active === "offroad"', role='presentation')
+                div(v-if='active === "offroad"')
                     h3 Off Roading
                     p I'm excited to explore the offroad in a Jeep, dirtbike or ATV.
                     p Exploring the back trails is only a success if Jeep comes back covered in more mud than paint.
 
-                div(v-if='active === "food"', role='presentation')
+                div(v-if='active === "food"')
                     h3 Food and Agriculture
                     p I am very interested agriculture and mass food production industries.
                     p When sourcing my food I try to purchase from local farmers where I can be assure of animal welfare or no artificial ingredients.
                     p I grow my own non-GMO, non-hybrid, heritage strains of herbs, fruits and vegetables.
 
-                div(v-if='active === "county"', role='presentation')
+                div(v-if='active === "county"')
                     h3 Country
                     p I a big fan of country culture.
                     p I listen country music and somehow end up at every country concert or festival that comes to town.
@@ -67,3 +73,18 @@
     }
   }
 </script>
+<style scoped>
+    .item {
+        width: 100%;
+        min-width: 40px;
+        cursor: pointer;
+        margin-bottom: 20px;
+        opacity: 1;
+        fill: #555;
+    }
+
+    .item:hover {
+        fill: #999;
+
+    }
+</style>
