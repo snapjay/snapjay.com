fbfa35644c5ba457fed94ac9e9d7c0f1203569ac
snapjay <dan.shreim@logrhythm.com>
2024-07-02 22:12:03 -0500

    clean up

--- concept/src/Panels/ContactPanel.vue
+++ concept/src/Panels/ContactPanel.vue
@@ -1,48 +1,47 @@
 <template>
-	<h1>Contact</h1>
-	<h2>Lets be friends!</h2>
-	<ul>
-		<li v-for="channel in connect" :key="channel.id">
-			<a :href="channel.href" :alt="channel.title" target="_blank" rel="noopener noreferrer">
-				<img :src="`/assets/img/${channel.img}`" :alt="channel.title" />
-			</a>
-		</li>
-	</ul>
+  <h1>Contact</h1>
+  <h2>Lets be friends!</h2>
+  <ul>
+    <li v-for="channel in connect" :key="channel.id">
+      <a :href="channel.href" :alt="channel.title" target="_blank" rel="noopener noreferrer">
+        <img :src="`/assets/img/${channel.img}`" class="grow" :alt="channel.title" />
+      </a>
+    </li>
+  </ul>
 </template>
 <script setup>
 const connect = [
-	{
-		id: 1,
-		img: "/connect/email.svg",
-		title: "Email",
-		href: "mailto:dan@snapjay.com",
-	},
-	{
-		id: 2,
-		img: "/connect/linkedin.svg",
-		title: "LinkedIn",
-		href: "https://www.linkedin.com/profile/view?id=18683312",
-	},
-	{
-		id: 3,
-		img: "/connect/github.svg",
-		title: "Github",
-		href: "http://github.com/snapjay",
-	},
-	{
-		id: 4,
-		img: "/connect/x.svg",
-		title: "X",
-		href: "https://x.com/snapjay",
-	},
-];
-
+  {
+    id: 1,
+    img: '/connect/email.svg',
+    title: 'Email',
+    href: 'mailto:dan@snapjay.com'
+  },
+  {
+    id: 2,
+    img: '/connect/linkedin.svg',
+    title: 'LinkedIn',
+    href: 'https://www.linkedin.com/profile/view?id=18683312'
+  },
+  {
+    id: 3,
+    img: '/connect/github.svg',
+    title: 'Github',
+    href: 'http://github.com/snapjay'
+  },
+  {
+    id: 4,
+    img: '/connect/x.svg',
+    title: 'X',
+    href: 'https://x.com/snapjay'
+  }
+]
 </script>
 <style scoped>
 img {
-	width: 250px;
-	height: auto;
-	margin: 1.5rem auto;
-	display: block;
+  width: 250px;
+  height: auto;
+  margin: 1.5rem auto;
+  display: block;
 }
 </style>
--- concept/src/Panels/CreditsPanel.vue
+++ concept/src/Panels/CreditsPanel.vue
@@ -2,61 +2,61 @@
 const clients = [
   {
     id: 1,
-    img: "credits/Telus-Logo.svg",
-    title: "Telus",
+    img: 'credits/Telus-Logo.svg',
+    title: 'Telus',
     desc: "Developed the user interface for Telus' internal employee loyalty and rewards program's frontend components.",
-    href: "https://www.telus.com/",
+    href: 'https://www.telus.com/'
   },
   {
     id: 2,
-    img: "credits/EBay_logo.svg",
-    title: "ebay",
+    img: 'credits/EBay_logo.svg',
+    title: 'ebay',
     desc: 'Created a prototype for a "Community Court" concept aimed at effectively resolving conflicts between customers.',
-    href: "https://www.ebay.co.uk/",
+    href: 'https://www.ebay.co.uk/'
   },
   {
     id: 3,
-    img: "credits/LogRhythmLogo.svg",
-    title: "LogRhythm",
+    img: 'credits/LogRhythmLogo.svg',
+    title: 'LogRhythm',
     desc: "Senior frontend developer role working in the advancement of the company's flagship security product by contributing to its development and infrastructure enhancement efforts.",
-    href: "https://logrhythm.com/",
+    href: 'https://logrhythm.com/'
   },
   {
     id: 4,
-    img: "credits/Lonely_Planet-Logo.svg",
-    title: "Lonely Planet",
-    desc: "Developed a prototype for the travel guide website, incorporating essential features and functionalities.",
-    href: "https://www.lonelyplanet.com/places",
+    img: 'credits/Lonely_Planet-Logo.svg',
+    title: 'Lonely Planet',
+    desc: 'Developed a prototype for the travel guide website, incorporating essential features and functionalities.',
+    href: 'https://www.lonelyplanet.com/places'
   },
   {
     id: 5,
-    img: "credits/Top_Gear_29.webp",
-    title: "Top Gear",
-    desc: "Built concept protoype for a car selling application, intended for the purpose of conducting user testing.",
-    href: "https://www.topgear.com/",
+    img: 'credits/Top_Gear_29.webp',
+    title: 'Top Gear',
+    desc: 'Built concept protoype for a car selling application, intended for the purpose of conducting user testing.',
+    href: 'https://www.topgear.com/'
   },
   {
     id: 8,
-    img: "credits/Intel_logo.svg",
-    title: "Intel",
-    desc: "Built a tempoary micro site for a promotional back-to-school campaign contest partnership between Intel and Staples.",
-    href: "https://www.intel.com/",
+    img: 'credits/Intel_logo.svg',
+    title: 'Intel',
+    desc: 'Built a tempoary micro site for a promotional back-to-school campaign contest partnership between Intel and Staples.',
+    href: 'https://www.intel.com/'
   },
   {
     id: 6,
-    img: "credits/Toronto-Dominion_Bank.svg",
-    title: "TD Bank",
+    img: 'credits/Toronto-Dominion_Bank.svg',
+    title: 'TD Bank',
     desc: "Participated in the development of the company's customer loyalty program, enabling customers to efficiently redeem accumulated points for various products.",
-    href: "https://www.td.com/ca/en/personal-banking/",
+    href: 'https://www.td.com/ca/en/personal-banking/'
   },
   {
     id: 7,
-    img: "credits/engage_red_logo.png",
-    title: "Engage People",
-    desc: "Served in the capacity of Director of Product Development at Engage, spearheading innovation, internal product development, and client-focused initiatives.",
-    href: "https://www.engagepeople.com/",
-  },
-]  
+    img: 'credits/engage_red_logo.png',
+    title: 'Engage People',
+    desc: 'Served in the capacity of Director of Product Development at Engage, spearheading innovation, internal product development, and client-focused initiatives.',
+    href: 'https://www.engagepeople.com/'
+  }
+]
 </script>
 
 <template>
@@ -66,7 +66,7 @@ const clients = [
     <li v-for="client in clients.sort((a, b) => a.title.localeCompare(b.title))" :key="client.id">
       <hr />
       <a :href="client.href" target="_blank" rel="noopener noreferrer">
-        <img :src="`/assets/img/${client.img}`" :alt="client.title" />
+        <img :src="`/assets/img/${client.img}`" class="grow" :alt="client.title" />
       </a>
       <p>{{ client.desc }}</p>
     </li>
