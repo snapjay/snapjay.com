8528a366d6d73a629e940018d5a40c58e967dd40
snapjay <dan@snapjay.com>
2024-07-22 12:45:05 -0500

    Fix Build issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
