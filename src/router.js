c1e024ce95d7adacfddc0de68922f9c149710b8f
snapjay <dan@snapjay.com>
2026-06-09 19:29:59 -0400

    Add GA

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import words from './words.json'
 
 const routes = [
   {
@@ -12,6 +13,32 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+const DEFAULT_TITLE = 'snapjay | Engineer + Entrepreneur | Knoxville, TN'
+
+router.afterEach((to) => {
+  const id = to.params.id
+  let title = DEFAULT_TITLE``
+
+  if (id) {
+    const word = words.find(w => w.id === id)
+    if (word) {
+      title = `snapjay | ${word.label}`
+    }
+  }
+
+  // Update document title
+  document.title = title
+
+  // Track page view in Google Analytics
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', 'page_view', {
+      page_location: window.location.href,
+      page_path: to.fullPath,
+      page_title: title
+    })
+  }
 })
 
 export default router
